9654966ee58e9713904b15b72a1216da31a70378
KonnosDev <109918763+KonnosDev@users.noreply.github.com>
2022-08-06 13:18:50 +0300

    Add files via upload

---
+++ Koncrypt/enlib.py
@@ -0,0 +1,133 @@
+def K_enc(word):
+    enc_word= ""
+    for char in word:
+            if char == "A" :
+                    enc_word = enc_word + "#"
+            elif char == "B" :
+                    enc_word = enc_word + "!"
+            elif char == "C" :
+                    enc_word = enc_word + ":"
+            elif char == "D" :
+                    enc_word = enc_word + "%"
+            elif char == "E" :
+                    enc_word = enc_word + ";"
+            elif char == "F" :
+                    enc_word = enc_word + "@"
+            elif char == "G" :
+                    enc_word = enc_word + "€"
+            elif char == "H" :
+                    enc_word = enc_word + "/"
+            elif char == "I" :
+                    enc_word = enc_word + "["
+            elif char == "J" :
+                    enc_word = enc_word + "{"
+            elif char == "K" :
+                    enc_word = enc_word + "("
+            elif char == "L" :
+                    enc_word = enc_word + "]"
+            elif char == "M" :
+                    enc_word = enc_word + "}"
+            elif char == "N" :
+                    enc_word = enc_word + ")"
+            elif char == "O" :
+                    enc_word = enc_word + "="
+            elif char == "P" :
+                    enc_word = enc_word + "<"
+            elif char == "Q" :
+                    enc_word = enc_word + "?"
+            elif char == "R" :
+                    enc_word = enc_word + "+"
+            elif char == "S" :
+                    enc_word = enc_word + "-"
+            elif char == "T" :
+                    enc_word = enc_word + ">"
+            elif char == "U" :
+                    enc_word = enc_word + "_"
+            elif char == "V" :
+                    enc_word = enc_word + "*"
+            elif char == "W" :
+                    enc_word = enc_word + "|"
+            elif char == "X" :
+                    enc_word = enc_word + "&"
+            elif char == "Y" :
+                    enc_word = enc_word + "~"
+            elif char == "Z":
+                    enc_word = enc_word + "$"
+            elif char == "a" :
+                    enc_word = enc_word + "ς"
+            elif char == "b" :
+                    enc_word = enc_word + "μ"
+            elif char == "c" :
+                    enc_word = enc_word + "ε"
+            elif char == "d" :
+                    enc_word = enc_word + "ν"
+            elif char == "e" :
+                    enc_word = enc_word + "ρ"
+            elif char == "f" :
+                    enc_word = enc_word + "β"
+            elif char == "g" :
+                    enc_word = enc_word + "ω"
+            elif char == "h" :
+                    enc_word = enc_word + "τ"
+            elif char == "i" :
+                    enc_word = enc_word + "ψ"
+            elif char == "j" :
+                    enc_word = enc_word + "υ"
+            elif char == "k" :
+                    enc_word = enc_word + "θ"
+            elif char == "l" :
+                    enc_word = enc_word + "ζ"
+            elif char == "m" :
+                    enc_word = enc_word + "χ"
+            elif char == "n" :
+                    enc_word = enc_word + "π"
+            elif char == "o" :
+                    enc_word = enc_word + "ι"
+            elif char == "p" :
+                    enc_word = enc_word + "α"
+            elif char == "q" :
+                    enc_word = enc_word + "λ"
+            elif char == "r" :
+                    enc_word = enc_word + "σ"
+            elif char == "s" :
+                    enc_word = enc_word + "δ"
+            elif char == "t" :
+                    enc_word = enc_word + "κ"
+            elif char == "u" :
+                    enc_word = enc_word + "ξ"
+            elif char == "v" :
+                    enc_word = enc_word + "η"
+            elif char == "w" :
+                    enc_word = enc_word + "γ"
+            elif char == "x" :
+                    enc_word = enc_word + "δ"
+            elif char == "y" :
+                    enc_word = enc_word + "£"
+            elif char == "z" :
+                    enc_word = enc_word + "Η"
+            elif char == "0" :
+                    enc_word =enc_word + "Α"
+            elif char == "1" :
+                    enc_word =enc_word + "Μ"
+            elif char == "2" :
+                    enc_word =enc_word + "Π"
+            elif char == "3" :
+                    enc_word =enc_word + "Ο"
+            elif char == "4" :
+                    enc_word =enc_word + "Ξ"
+            elif char == "5" :
+                    enc_word =enc_word + "Ε"
+            elif char == "6" :
+                    enc_word =enc_word + "Ρ"
+            elif char == "7" :
+                    enc_word =enc_word + "Β"
+            elif char == "8" :
+                    enc_word =enc_word + "Χ"
+            elif char == "9" :
+                    enc_word =enc_word + "Ζ"
+            else:
+                    enc_word =enc_word + char
+    return enc_word
+
+            
+            
